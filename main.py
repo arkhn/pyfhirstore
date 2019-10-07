5a1c2a6cae503d0cc4c9b71f9770b30f68d7571b
Simon Vadée <simon@stratumn.com>
2019-10-07 17:52:45 +0200

    Schema parser and database setup (#1)

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+import json
+from timeit import default_timer as timer
+from pymongo import MongoClient
+
+from fhirstore import FHIRStore
+
+if __name__ == '__main__':
+    client = MongoClient()
+    store = FHIRStore(client, "fhirstore")
+
+    # reset collections
+    print("Dropping collections...")
+    start = timer()
+    store.reset()
+    end = timer()
+    print(end - start, "seconds")
+
+    # create collections
+    print("Creating collections...")
+    start = timer()
+    store.bootstrap("schema/fhir.schema.json", depth=5)
+    end = timer()
+    print(end - start, "seconds")
+
+    # creating document
+    print("Inserting document...")
+    start = timer()
+    store.create({
+        "resourceType": "Patient",
+        "gender": "male",
+    })
+    end = timer()
+    print(end - start, "seconds")
