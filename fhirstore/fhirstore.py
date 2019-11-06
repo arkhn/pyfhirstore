444e93db3381bed99f6a66fd0a36afa127489871
Simon Vadée <simon.vadee@gmail.com>
2019-11-06 16:26:21 +0100

    Handle FHIR extensions (fixes #3) (#4)
    * Handle FHIR extensions (fixes #3)
    
    * Allow fhirstore toget existing collections from the db
    
    * add patch method and bump version 0.0.6
    
    * init Fhirstore with resources

--- fhirstore/fhirstore.py
+++ fhirstore/fhirstore.py
@@ -9,6 +9,7 @@ from pymongo.errors import (
     OperationFailure,
 )
 from tqdm import tqdm
+from jsonschema import validate
 
 from fhirstore.schema import SchemaParser
 
@@ -28,9 +29,14 @@ class BadRequestError(Exception):
 
 
 class FHIRStore:
-    def __init__(self, client: MongoClient, db_name: str):
+    def __init__(
+            self,
+            client: MongoClient,
+            db_name: str,
+            resources: dict = {}):
         self.db = client[db_name]
         self.parser = SchemaParser()
+        self.resources = resources
 
     def reset(self):
         """
@@ -38,8 +44,9 @@ class FHIRStore:
         """
         for collection in self.db.list_collection_names():
             self.db.drop_collection(collection)
+        self.resources = {}
 
-    def bootstrap(self, depth=4, resource=None, show_progress=True):
+    def bootstrap(self, depth=3, resource=None, show_progress=True):
         """
         Parses the FHIR json-schema and create the collections according to it.
         """
@@ -48,8 +55,6 @@ class FHIRStore:
             tqdm.write("\n", end="")
             resources = tqdm(
                 resources,
-                total=len(self.parser.resources),
-                leave=False,
                 file=sys.stdout,
                 desc="Bootstrapping collections...",
             )
@@ -57,12 +62,32 @@ class FHIRStore:
             ret = self.db.create_collection(
                 resource_name, **{"validator": {"$jsonSchema": schema}}
             )
+            self.resources[resource_name] = schema
+
+    def resume(self, show_progress=True):
+        """
+        Loads the existing resources schema from the database.
+        """
+        collections = self.db.list_collection_names()
+
+        if show_progress:
+            tqdm.write("\n", end="")
+            collections = tqdm(
+                collections,
+                file=sys.stdout,
+                desc="Loading collections from database...",
+            )
+
+        for collection in collections:
+            json_schema = self.db.get_collection(
+                collection).options()['validator']['$jsonSchema']
+            self.resources[collection] = json_schema
 
     def validate_resource_type(self, resource_type):
         if resource_type is None:
             raise BadRequestError("resourceType is missing in resource")
 
-        elif resource_type not in self.db.list_collection_names():
+        elif resource_type not in self.resources:
             raise NotFoundError(
                 f'unsupported FHIR resource: "{resource_type}"'
             )
@@ -86,7 +111,7 @@ class FHIRStore:
             res = self.db[resource_type].insert_one(resource)
             return {**resource, "_id": res.inserted_id}
         except WriteError as err:
-            self.parser.validate(resource)
+            self.validate(resource)
 
     def read(self, resource_type, resource_id):
         """
@@ -106,7 +131,36 @@ class FHIRStore:
             raise NotFoundError
         return res
 
-    def update(self, resource_type, resource_id, patch):
+    def update(self, resource_type, resource_id, resource):
+        """
+        Update a resource given its type, id and a resource. It applies
+        a "replace" operation, therefore the resource will be overriden.
+        The structure of the updated resource will  be checked against
+        its json-schema FHIR definition.
+
+        Args:
+            - resource_type: type of the resource (eg: 'Patient')
+            - id: The expected id is the resource 'id', not the
+                  internal database identifier ('_id').
+            - resource: The updated resource.
+
+        Returns: The updated resource.
+        """
+        self.validate_resource_type(resource_type)
+
+        try:
+            updated = self.db[resource_type].find_one_and_replace(
+                {"id": resource_id},
+                resource,
+                return_document=ReturnDocument.AFTER,
+            )
+            if updated is None:
+                raise NotFoundError
+            return updated
+        except OperationFailure as err:
+            self.validate(resource)
+
+    def patch(self, resource_type, resource_id, patch):
         """
         Update a resource given its type, id and a patch. It applies
         a "patch" operation rather than a "replace", only the fields
@@ -118,6 +172,7 @@ class FHIRStore:
             - resource_type: type of the resource (eg: 'Patient')
             - id: The expected id is the resource 'id', not the
                   internal database identifier ('_id').
+            - patch: The patch to be applied on the resource.
 
         Returns: The updated resource.
         """
@@ -133,7 +188,8 @@ class FHIRStore:
                 raise NotFoundError
             return updated
         except OperationFailure as err:
-            self.parser.validate({**patch, "resourceType": resource_type})
+            self.read(resource_type, resource_id)
+            self.validate({**self.resource, **patch})
 
     def delete(self, resource_type, resource_id):
         """
@@ -152,3 +208,20 @@ class FHIRStore:
         if res.deleted_count == 0:
             raise NotFoundError
         return resource_id
+
+    def validate(self, resource):
+        """
+        Validates the given resource against its own schema.
+        This is much more efficient than running th validation against the
+        whole FHIR json schema.
+        This function is useful because MongoDB does not provide any feedback
+        about the schema validation error other than "Schema validation failed"
+
+        Args:
+            resource: The object to be validated against the schema.
+                      It is expected to have the "resourceType" property.
+        """
+        schema = self.resources.get(resource["resourceType"])
+        if schema is None:
+            raise Exception(f"missing schema for resource {resource}")
+        validate(instance=resource, schema=schema)
