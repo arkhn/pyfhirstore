0c323506f0e0c4358c55c741c00424c79e602caa
Simon Vadée <simon.vadee@gmail.com>
2019-11-06 16:26:21 +0100

    Handle FHIR extensions (fixes #3) (#4)
    * Handle FHIR extensions (fixes #3)
    
    * Allow fhirstore toget existing collections from the db
    
    * add patch method and bump version 0.0.6
    
    * init Fhirstore with resources

--- fhirstore/schema/parser.py
+++ fhirstore/schema/parser.py
@@ -2,34 +2,11 @@ import os
 import json
 from copy import deepcopy
 
-from jsonschema import validate
 
-# resource_blacklist contains resources that should not be taken into account
-# while parsing the whole schema
-resource_blacklist = [
-    # resource 'Parameters' contains all other resources
-    # and is too big (144Mo with depth=5). As the 'Parameters' resource is
-    # only used at the API level, it should not be persisted in storage and is
-    # therefore blacklisted.
-    "Parameters"
+# Do not resolve the following resources when parsing the schema.
+definitions_blacklist = [
+    'ResourceList'  # ResourceList embeds all other resources,
 ]
-
-
-def clean_resource(resource):
-    """
-    Removes unwanted fields from the resource properties.
-    As the following properties result in a way too big schema,
-    we decided to ignore them for now:
-         - extension
-         - modifierExtension
-         - contained
-    """
-    if "extension" in resource["properties"]:
-        del resource["properties"]["extension"]
-    if "modifierExtension" in resource["properties"]:
-        del resource["properties"]["modifierExtension"]
-    if "contained" in resource["properties"]:
-        del resource["properties"]["contained"]
 
 
 def compatibilize_schema(resource):
@@ -63,11 +40,10 @@ class SchemaParser:
         with open(schema_path, "r") as schemaFile:
             self.schema = json.load(schemaFile)
             self.definitions = self.schema["definitions"]
-            self.resources = {
-                os.path.basename(r["$ref"]): None
+            self.resources = [
+                os.path.basename(r["$ref"])
                 for r in self.schema["oneOf"]
-                if os.path.basename(r["$ref"]) not in resource_blacklist
-            }
+            ]
 
     def parse(self, resource=None, depth=4):
         """
@@ -82,12 +58,12 @@ class SchemaParser:
                     (resource_name, schema)
         """
         if resource is None:
-            for resource in self.resources.keys():
-                self.resources[resource] = self.parse_schema(resource, depth)
-                yield (resource, self.resources[resource])
+            for resource in self.resources:
+                schema = self.parse_schema(resource, depth)
+                yield (resource, schema)
         else:
-            self.resources[resource] = self.parse_schema(resource, depth)
-            yield (resource, self.resources[resource])
+            schema = self.parse_schema(resource, depth)
+            yield (resource, schema)
 
     def parse_schema(self, resource: str, depth: int):
         """
@@ -102,10 +78,6 @@ class SchemaParser:
         Returns: The dereferenced schema.
                  (ready to be passed as mongoDB 'validator')
         """
-        if resource in resource_blacklist:
-            raise Exception(
-                f"Resource {resource} is blacklisted! Aborting the mission."
-            )
 
         r = self.definitions[resource]
         dereferenced_schema = self.resolve(r, depth)
@@ -129,59 +101,45 @@ class SchemaParser:
 
         """
         # stop resolving references if maximal depth is reached
+        # the reference is removed from the resource but all other
+        # properties are kept.
         if depth == 0 and "$ref" in resource:
-            return None
+            del resource["$ref"]
+            return resource
 
         # resource is a reference
-        # resolve its definition
+        # resolve its definition if not blacklisted.
         # (NB: deepcopy is required, resource will be modified in place)
         elif "$ref" in resource:
             path = os.path.basename(resource["$ref"])
-            return self.resolve(deepcopy(self.definitions[path]), depth - 1)
+            if path not in definitions_blacklist:
+                return self.resolve(
+                    deepcopy(self.definitions[path]), depth - 1)
+            del resource["$ref"]
 
         # resource is an object
         # resolve each of its properties recursively
         elif "properties" in resource:
-            clean_resource(resource)
             for k in list(resource["properties"].keys()):
-                resolved = self.resolve(resource["properties"][k], depth)
-                if resolved:
-                    resource["properties"][k] = resolved
+                # only one level of recursion is allowed for extensions
+                if k == "extension" or k == "modifierExtension":
+                    resource["properties"][k] = self.resolve(
+                        resource["properties"][k], depth if depth < 1 else 1)
                 else:
-                    del resource["properties"][k]
+                    resource["properties"][k] = self.resolve(
+                        resource["properties"][k], depth)
 
         # resource is an array
         # resolve its "items" property
         elif "items" in resource:
             resource["items"] = self.resolve(resource["items"], depth)
-            if resource["items"] is None:
-                return None
 
         # resource is a "oneOf" object
         # resolve each of its "oneOf" properties
         elif "oneOf" in resource:
-            resource["oneOf"] = list(
-                map(lambda x: self.resolve(x, depth), resource["oneOf"])
-            )
-            if len(list(filter(lambda x: x, resource["oneOf"]))) == 0:
-                return None
+            resource["oneOf"] = [
+                self.resolve(x, depth) for x in resource["oneOf"]
+            ]
 
         compatibilize_schema(resource)
         return resource
-
-    def validate(self, resource):
-        """
-        Validates the given resource against its own schema.
-        This is much more efficient than running th validation against the
-        whole FHIR json schema.
-        This function is useful because MongoDB does not provide any feedback
-        about the schema validation error other than "Schema validation failed"
-
-        Args:
-            resource: The object to be validated against the schema.
-                      It is expected to have the "resourceType" property.
-        """
-        schema = self.resources.get(resource["resourceType"])
-        if schema is None:
-            raise Exception("missing schema")
-        validate(instance=resource, schema=schema)
--- main.py
+++ main.py
@@ -19,18 +19,26 @@ if __name__ == '__main__':
     # create collections
     print("Creating collections...")
     start = timer()
-    store.bootstrap(depth=5)
+    store.bootstrap(depth=3)
     end = timer()
     print(end - start, "seconds")
 
     # creating document
-    print("Inserting document...")
-    start = timer()
-    store.create({
-        "resourceType": "Patient",
-        "gender": "male",
-    })
-    end = timer()
-    print(end - start, "seconds")
+    print("Inserting documents...")
+    json_folder_path = os.path.join(os.getcwd(), "test/fixtures")
+    json_files = [x for x in os.listdir(
+        json_folder_path) if x.endswith(".json")]
+    for json_file in json_files:
+        json_file_path = os.path.join(json_folder_path, json_file)
+        with open(json_file_path, "r") as f:
+            data = json.load(f)
+            start = timer()
+            print("Creating", json_file_path, "...")
+            try:
+                store.create(data)
+            except Exception as e:
+                print(e)
+            end = timer()
+            print(end - start, "seconds")
 
     client.close()
