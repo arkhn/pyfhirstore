070a62843c7536ac865ea9b5669b85410aad8bb4
simonvadee <simon.vadee@gmail.com>
2019-11-08 11:51:23 +0100

    README and format output

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -5,13 +5,16 @@ import json
 from os import path
 from timeit import default_timer as timer
 import statistics
+from uuid import uuid4
 
 from tqdm import tqdm
 from pymongo import MongoClient
 from fhirstore import FHIRStore
 
-example_blacklist = ["package-min-ver.json", "profiles-resources.json",
-                     "questionnaireresponse-extensions-QuestionnaireResponse-item-subject.json"]
+example_blacklist = [
+    "package-min-ver.json",
+    "profiles-resources.json",
+    "questionnaireresponse-extensions-QuestionnaireResponse-item-subject.json"]
 
 
 def count_examples():
@@ -35,12 +38,10 @@ def download_resources():
     Downloads examples from HL7 website.
     """
     if not path.exists('benchmark/examples.zip'):
-        url = "http://www.hl7.org/fhir/examples-json.zip"  # big file test
-        # Streaming, so we can iterate over the response.
+        url = "http://www.hl7.org/fhir/examples-json.zip"
         r = requests.get(url, stream=True)
-        # Total size in bytes.
         total_size = int(r.headers.get('content-length', 0))
-        block_size = 1024  # 1 Kibibyte
+        block_size = 1024
         t = tqdm(total=total_size, unit='B', unit_scale=True,
                  desc="Downloading example resources")
         with open('benchmark/examples.zip', 'wb') as f:
@@ -55,34 +56,32 @@ def download_resources():
 download_resources()
 client = MongoClient()
 store = FHIRStore(client, "benchmark")
-# print("Removing previous collections")
 store.resume()
 # store.reset()
 # store.bootstrap(depth=3)
 
-print("WRITE :")
 examples = tqdm(iter_examples(),
                 total=count_examples(),
                 desc="Running write benchmark")
 stats = {}
 inserted = []
 for example, data in examples:
+    if not data.get('id'):
+        data["id"] = str(uuid4())
     start = timer()
     res = store.create(data)
     end = timer()
     stats[example] = end - start
     inserted.append(res)
 
-print(len(stats))
 values = stats.values()
-print(f"insertions per second (on average):", 1/statistics.mean(values))
-print(f"average:{statistics.mean(values)} milliseconds")
-print(f"median: {statistics.median(values)} milliseconds")
-print(f"min: {min(values)} milliseconds")
-print(f"max: {max(values)} milliseconds")
+print(f"insertions per second (on average): {1/statistics.mean(values):.2f}")
+print(f"average: {statistics.mean(values)*1000:.2f} milliseconds")
+print(f"median: {statistics.median(values)*1000:.2f} milliseconds")
+print(f"min: {min(values)*1000:.2f} milliseconds")
+print(f"max: {max(values)*1000:.2f} milliseconds")
 print(f"spread: {statistics.variance(values)}")
 
-print("READ :")
 examples = tqdm(inserted,
                 desc="Running read benchmark")
 stats = {}
@@ -90,13 +89,12 @@ for doc in examples:
     start = timer()
     store.read(doc["resourceType"], doc["id"])
     end = timer()
-    stats[example] = end - start
+    stats[doc["id"]] = end - start
 
-print(len(stats))
 values = stats.values()
-print(f"insertions per second (on average):", 1/statistics.mean(values))
-print(f"average:{statistics.mean(values)} milliseconds")
-print(f"median: {statistics.median(values)} milliseconds")
-print(f"min: {min(values)} milliseconds")
-print(f"max: {max(values)} milliseconds")
+print(f"reads per second (on average): {1/statistics.mean(values):.2f}")
+print(f"average: {statistics.mean(values)*1000:.2f} milliseconds")
+print(f"median: {statistics.median(values)*1000:.2f} milliseconds")
+print(f"min: {min(values)*1000:.2f} milliseconds")
+print(f"max: {max(values)*1000:.2f} milliseconds")
 print(f"spread: {statistics.variance(values)}")
