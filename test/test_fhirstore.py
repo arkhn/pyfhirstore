e6328865e1fccf448306cd391727405315901205
simonvadee <simon.vadee@gmail.com>
2019-11-25 12:09:50 +0100

    format tests

--- test/test_fhirstore.py
+++ test/test_fhirstore.py
@@ -19,18 +19,13 @@ class TestFHIRStore:
     def test_create_missing_resource_type(self, store: FHIRStore):
         """create() raises if resource type is not specified"""
 
-        with raises(
-            BadRequestError, match="resourceType is missing in resource"
-        ):
+        with raises(BadRequestError, match="resourceType is missing in resource"):
             store.create({})
 
     def test_create_bad_resource_type(self, store: FHIRStore):
         """create() raises if resource type is unknown"""
 
-        with raises(
-            NotFoundError,
-            match='unsupported FHIR resource: "unknown"',
-        ):
+        with raises(NotFoundError, match='unsupported FHIR resource: "unknown"'):
             store.create({"resourceType": "unknown"})
 
     def test_create_bad_resource_schema(self, store: FHIRStore):
@@ -39,13 +34,10 @@ class TestFHIRStore:
         with raises(ValidationError):
             store.create({"resourceType": "Patient", "id": 42})
 
-    def test_create_resource(
-        self, store: FHIRStore, mongo_client: MongoClient, test_patient
-    ):
+    def test_create_resource(self, store: FHIRStore, mongo_client: MongoClient, test_patient):
         """create() correctly inserts a document in the database"""
         result = store.create(test_patient)
-        assert isinstance(
-            result["_id"], ObjectId), "result _id must be an objectId"
+        assert isinstance(result["_id"], ObjectId), "result _id must be an objectId"
         inserted = mongo_client["Patient"].find_one({"_id": result["_id"]})
         assert inserted == test_patient
 
@@ -55,10 +47,10 @@ class TestFHIRStore:
         with open("test/fixtures/patient-example-with-extensions.json") as f:
             patient = json.load(f)
             result = store.create(patient)
-            assert isinstance(
-                result["_id"], ObjectId), "result _id must be an objectId"
+            assert isinstance(result["_id"], ObjectId), "result _id must be an objectId"
             inserted = mongo_client["Patient"].find_one({"_id": result["_id"]})
             assert inserted == patient
+
     ###
     # FHIRStore.read()
     ###
@@ -66,10 +58,7 @@ class TestFHIRStore:
     def test_read_bad_resource_type(self, store: FHIRStore):
         """read() raises if resource type is unknown"""
 
-        with raises(
-            NotFoundError,
-            match='unsupported FHIR resource: "unknown"',
-        ):
+        with raises(NotFoundError, match='unsupported FHIR resource: "unknown"'):
             store.read("unknown", "864321")
 
     def test_read_resource_not_found(self, store: FHIRStore, test_patient):
@@ -90,36 +79,26 @@ class TestFHIRStore:
     def test_update_bad_resource_type(self, store: FHIRStore):
         """update() raises if resource type is unknown"""
 
-        with raises(
-            NotFoundError,
-            match='unsupported FHIR resource: "unknown"',
-        ):
+        with raises(NotFoundError, match='unsupported FHIR resource: "unknown"'):
             store.update("unknown", "864321", {"gender": "other"})
 
     def test_update_resource_not_found(self, store: FHIRStore, test_patient):
         """update() returns None when no matching document was found"""
 
         with raises(NotFoundError):
-            store.update(
-                "Patient", test_patient["id"], {"name": "Patator"}
-            )
+            store.update("Patient", test_patient["id"], {"name": "Patator"})
 
     def test_update_bad_resource_schema(self, store: FHIRStore, test_patient):
         """update() raises if json schema validation failed in mongo"""
 
         store.create(test_patient)
         with raises(ValidationError):
-            store.update(
-                "Patient", test_patient["id"], {
-                    **test_patient, "gender": "elephant"}
-            )
+            store.update("Patient", test_patient["id"], {**test_patient, "gender": "elephant"})
 
     def test_update_resource(self, store: FHIRStore, test_patient):
         """update() finds a document in the database"""
         store.create(test_patient)
-        result = store.update(
-            "Patient", test_patient["id"], {**test_patient, "gender": "other"}
-        )
+        result = store.update("Patient", test_patient["id"], {**test_patient, "gender": "other"})
         assert result == {**test_patient, "gender": "other"}
 
     ###
@@ -128,35 +107,26 @@ class TestFHIRStore:
     def test_patch_bad_resource_type(self, store: FHIRStore):
         """patch() raises if resource type is unknown"""
 
-        with raises(
-            NotFoundError,
-            match='unsupported FHIR resource: "unknown"',
-        ):
+        with raises(NotFoundError, match='unsupported FHIR resource: "unknown"'):
             store.patch("unknown", "864321", {"gender": "other"})
 
     def test_patch_resource_not_found(self, store: FHIRStore, test_patient):
         """patch() returns None when no matching document was found"""
 
         with raises(NotFoundError):
-            store.patch(
-                "Patient", test_patient["id"], {"name": "Patator"}
-            )
+            store.patch("Patient", test_patient["id"], {"name": "Patator"})
 
     def test_patch_bad_resource_schema(self, store: FHIRStore, test_patient):
         """patch() raises if json schema validation failed in mongo"""
 
         store.create(test_patient)
         with raises(ValidationError):
-            store.patch(
-                "Patient", test_patient["id"], {"gender": "elephant"}
-            )
+            store.patch("Patient", test_patient["id"], {"gender": "elephant"})
 
     def test_patch_resource(self, store: FHIRStore, test_patient):
         """patch() finds a document in the database"""
         store.create(test_patient)
-        result = store.patch(
-            "Patient", test_patient["id"], {"gender": "other"}
-        )
+        result = store.patch("Patient", test_patient["id"], {"gender": "other"})
         assert result == {**test_patient, "gender": "other"}
 
     ###
@@ -165,10 +135,7 @@ class TestFHIRStore:
     def test_delete_bad_resource_type(self, store: FHIRStore):
         """delete() raises if resource type is unknown"""
 
-        with raises(
-            NotFoundError,
-            match='unsupported FHIR resource: "unknown"',
-        ):
+        with raises(NotFoundError, match='unsupported FHIR resource: "unknown"'):
             store.delete("unknown", "864321")
 
     def test_delete_resource_not_found(self, store: FHIRStore, test_patient):
