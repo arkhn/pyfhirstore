077a5a3bd30fb9fb919d1179d9499df4b9aaf487
simonvadee <simon.vadee@gmail.com>
2019-11-25 11:41:01 +0100

    publish workflow

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = read("requirements.txt").split()
 
 setuptools.setup(
     name="fhirstore",
-    version="0.0.6",
+    version="0.1.0",
     author="Arkhn",
     author_email="contact@arkhn.org",
     description="Manipulating FHIR data leveraging MongoDB as storage layer",
