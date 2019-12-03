341ff77d051ddb05314420af21f27ff63748f15d
Elsie Hoffet <elsie@arkhn.com>
2019-12-03 11:00:39 +0100

    Correct a typo in the replication set initiation script

--- scripts/initiate_rep_set.sh
+++ scripts/initiate_rep_set.sh
@@ -4,7 +4,7 @@
 i=0
 while [ $i -lt 10 ]; do
   curl -s -u elastic:SuperSecurePassword2019 'localhost:9200/_cluster/health?wait_for_status=yellow&timeout=10s' > /tmp/output
-  if [ $? -eq 0 ] && cat /tmp/output | jq -e "select(.timed_out == true)"; then
+  if [ $? -eq 0 ] && cat /tmp/output | jq -e "select(.timed_out == false)"; then
   docker exec pyfhirstore_mongo_1 mongo --username="arkhn" --password="SuperSecurePassword2019" --eval "rs.initiate()"
   break;
   fi
