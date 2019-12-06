ea463e2e54d656216a75ff40471c5b100160f326
Elsie Hoffet <elsie@arkhn.com>
2019-12-06 14:07:32 +0100

    Fix waiting time in Replica set initiation script

--- scripts/initiate_rep_set.sh
+++ scripts/initiate_rep_set.sh
@@ -2,16 +2,17 @@
 
 
 i=0
-while [ $i -lt 10 ]; do
+while [ $i -lt 20 ]; do
   curl -s -u elastic:SuperSecurePassword2019 'localhost:9200/_cluster/health?wait_for_status=yellow&timeout=10s' > /tmp/output
   if [ $? -eq 0 ] && cat /tmp/output | jq -e "select(.timed_out == false)"; then
   docker exec pyfhirstore_mongo_1 mongo --username="arkhn" --password="SuperSecurePassword2019" --eval "rs.initiate()"
   break;
   fi
-    sleep 1
+    echo "ElasticSearch is unavailable - sleeping" >&2
+    sleep 2
     i=$((i+1))
 done
-if [ $i -eq 10 ]; then exit 1; fi
+if [ $i -eq 20 ]; then exit 1; fi
 
 
 echo "Replica Set is initiated - resuming execution"
