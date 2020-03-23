e31be5c2fc0bcc8d077de981cf156508085cd6c1
Jasopaum <jason.paumier@gmail.com>
2020-03-23 10:56:17 +0100

    Bump version (#33)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = read("requirements.txt").split()
 
 setuptools.setup(
     name="fhirstore",
-    version="0.3.7",
+    version="0.3.8",
     author="Arkhn",
     author_email="contact@arkhn.org",
     description="Manipulating FHIR data leveraging MongoDB as storage layer",
